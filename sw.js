f4e53b7406a5a2a45c945c936940255a1cbc362d
Tamirys <tamirys@gec.inatel.br>
2020-12-12 19:36:55 -0300

    Implement HTML5 Cache API

--- sw.js
+++ sw.js
@@ -1,3 +1,30 @@
 self.addEventListener('install', event => {
     console.log('[Service Worker] Installing service worker...');
+    event.waitUntil(
+        caches.open('app-shell-v1')
+        .then(cache => {
+            return cache.addAll([
+                'offline.html'
+            ]);
+        })
+    );
+    self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+    console.log('[Service Worker] Activating service worker...');
+});
+
+async function networkFirst(request) {
+    try {
+        return await fetch(request);
+    } catch {
+        const cache = await caches.open('app-shell-v1');
+        return cache.match('offline.html');
+    }
+}
+
+self.addEventListener('fetch', event => {
+    /* console.log('[Service Worker] Fetch event...', event); */
+    event.respondWith(networkFirst(event.request));
 });
