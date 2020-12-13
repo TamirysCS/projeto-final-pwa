52fa641d304239381b974b4b333bc46a47e3be1f
Tamirys <tamirys@gec.inatel.br>
2020-12-13 20:40:04 -0300

    Add button listener

--- assets/js/HtmlService.js
+++ assets/js/HtmlService.js
@@ -1,10 +1,17 @@
 export default class HtmlService {
 
 constructor(){
-
+    this.bindButtonListener();
 }
 
+bindButtonListener() {
 
+    const button = document.querySelector("button");
+    button.addEventListener("click", () => {
+      console.log("Button clicked!");
+      
+    });
+}
 
 
 }
